11dfb02ce3b3cf5d22b99b64b08ca2571b30b8eb
Adamka00 <adam.majczan@gmail.com>
2023-03-12 12:05:56 +0100

    Created

---
+++ end.js
@@ -0,0 +1,9 @@
+document.getElementById("catBtnEnd").onclick = function(){
+    alert("It's Cat! Yeeey!");
+    window.location.href = "end.html";
+  }
+  
+  document.getElementById("cumBtnEnd").onclick = function(){
+    alert("It's Cum! Yeeey!");
+    window.location.href = "end.html";
+  }
---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("cumBtn").onclick = function(){
+  alert("It's Cum! Yeeey!");
+  window.location.href = "game2.html";
+}
+
+document.getElementById("catBtn").onclick = function(){
+  alert("It's Cat! Yeeey!");
+  window.location.href = "game2.html";
+}
