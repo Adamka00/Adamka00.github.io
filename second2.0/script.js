fa6345c1973e8cf015b9cdaa660698de553621b9
Adamka00 <adam.majczan@gmail.com>
2024-11-20 16:34:09 +0100

    asd2.02.0

--- second2.0/script.js
+++ second2.0/script.js
@@ -1,3 +1,25 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key == 'c') {
+        const curw = parseInt(getComputedStyle(square).width)
+        const curh = parseInt(getComputedStyle(square).height)
+
+        square.style.width = (curw + 10) + 'px'
+        square.style.height = (curh + 10) + 'px'
+
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+        square.style.backgroundColor = randomColor;
+    } else if (event.key == 'v') {
+        const curw = parseInt(getComputedStyle(square).width)
+        const curh = parseInt(getComputedStyle(square).height)
+
+        square.style.width = (curw - 10) + 'px'
+        square.style.height = (curh - 10) + 'px'
+
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+        square.style.backgroundColor = randomColor;
+    }
+})
+
 document.addEventListener("DOMContentLoaded", function() {
 
     const square = document.getElementById('square')
